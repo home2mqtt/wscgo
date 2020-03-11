af30e88fa9500543af558706959604d8c1f7fc82
Balázs Grill <balazs.grill@incquerylabs.com>
2020-03-11 08:21:36 +0100

    #10 use periph's tools to extract platform info

--- config/info_linux.go
+++ config/info_linux.go
@@ -2,11 +2,9 @@
 package config
 
 import (
-	"bufio"
-	"errors"
 	"io/ioutil"
-	"os"
-	"strings"
+
+	"periph.io/x/periph/host/distro"
 )
 
 const modelinfo string = "/proc/device-tree/model"
@@ -17,20 +15,14 @@ func getModelInfo() (string, string, error) {
 	var model string
 	if err != nil {
 		// Cannot read model file, assume generic linux
-		model = "linux"
+		model = distro.OSRelease()["PRETTY_NAME"]
 	}
 	model = string(modelbytes)
-	cpuinfofile, err := os.Open(cpuinfo)
-	scanner := bufio.NewScanner(cpuinfofile)
-	for scanner.Scan() {
-		line := scanner.Text()
-		if strings.Contains(line, ":") {
-			keyvalue := strings.Split(line, ":")
-			if strings.ToLower(strings.TrimSpace(keyvalue[0])) == "serial" {
-				serialstring := strings.TrimSpace(keyvalue[1])
-				return model, serialstring, nil
-			}
-		}
+
+	var serial string
+	var ok bool
+	if serial, ok = distro.CPUInfo()["serial"]; !ok {
+		serial = "Unkown"
 	}
-	return "", "", errors.New("Invalid serial number")
+	return model, serial, nil
 }
