110f0eb00b8fb4c0b3d7663855f9fdc45a3ff80b
Balázs Grill <balazs.grill@incquerylabs.com>
2020-03-15 08:20:49 +0100

    fix

--- plugins/wiringpi/wiringpiplugin.go
+++ plugins/wiringpi/wiringpiplugin.go
@@ -3,7 +3,7 @@ package main
 import "gitlab.com/grill-tamasi/wscgo/plugins"
 
 func GetAddons() []plugins.Addon {
-	return {
+	return []plugins.Addon{
 		&mcp23017addon{},
 		&pcapca9685addon{},
 	}
